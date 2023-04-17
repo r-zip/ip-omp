67a2b9e4f99141c4c6d00302728ed05f6d4094ff
Ryan Pilgrim <rzpilgrim@gmail.com>
2023-04-17 15:02:01 -0400

    buggy

--- omp.py
+++ omp.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from copy import deepcopy
 from pathlib import Path
 from typing import Any
@@ -7,15 +8,22 @@ import matplotlib.pyplot as plt
 import torch
 import seaborn as sns
 import typer
+from rich.logging import RichHandler
 
-plt.rcParams.update(
-    {
-        "text.usetex": True,
-        "font.family": "Helvetica",
-    }
+logging.basicConfig(
+    level=logging.INFO, format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
 )
+logger = logging.getLogger()
+
+# plt.rcParams.update(
+#     {
+#         "text.usetex": True,
+#         "font.family": "Helvetica",
+#     }
+# )
 
 device_: str | torch.device = "cpu"
+log_frequency_: int | None = None
 
 
 def gen_dictionary(m: int, n: int) -> torch.Tensor:
@@ -129,7 +137,10 @@ def ip(
 ) -> Log:
     log = Log(debug=debug)
     indices = []
+    k = 0
     while True:
+        if log_frequency_ is not None and (k + 1) % log_frequency_ == 0:
+            logger.debug(f"Starting OMP iteration {k}.")
         objective = ip_objective(Phi, y, indices=indices)
         max_objective = objective.max()
         log.log("objective", max_objective.item())
@@ -141,6 +152,7 @@ def ip(
         log.log("y_hat", y_hat)
         x_hat = ip_estimate_x(Phi, indices, y)
         log.log("x_hat", x_hat)
+        k += 1
 
     return log
 
@@ -153,7 +165,10 @@ def omp(
 ) -> Log:
     log = Log(debug=debug)
     indices = []
+    k = 0
     while True:
+        if log_frequency_ is not None and (k + 1) % log_frequency_ == 0:
+            logger.info(f"Starting OMP iteration {k}.")
         P = projection(Phi[:, indices], perp=True)
         residual = P @ y
         squared_error = residual.T @ residual
@@ -167,6 +182,7 @@ def omp(
         log.log("y_hat", y_hat)
         x_hat = omp_estimate_x(Phi, indices, y)
         log.log("x_hat", x_hat)
+        k += 1
 
     return log
 
@@ -183,8 +199,20 @@ def mse(estimated: torch.Tensor, true: torch.Tensor) -> float:
     return torch.mean((estimated - true) ** 2).item()
 
 
-def main(m: int, n: int, s: float, output_dir: Path, device: str | None = None):
+def main(
+    m: int,
+    n: int,
+    s: float,
+    output_dir: Path,
+    device: str | None = None,
+    plot: bool = False,
+    log_frequency: int | None = None,
+):
     global device_
+    global log_frequency_
+    log_frequency_ = log_frequency
+    if log_frequency_:
+        logger.setLevel(logging.DEBUG)
 
     if device is None:
         if torch.cuda.is_available():
@@ -194,16 +222,23 @@ def main(m: int, n: int, s: float, output_dir: Path, device: str | None = None):
         else:
             device = "cpu"
 
+    logger.info(f"Using device {device}.")
+
     with torch.no_grad():
         output_dir.mkdir(exist_ok=True)
+        logger.info(f"Saving to {output_dir}.")
 
         sns.set()
 
+        logger.info(f"Generating dictionary of size {(m, n)}")
         Phi = gen_dictionary(m, n)
         y, x = generate_measurements_and_coeffs(Phi, p=s)
         true_support = set(torch.where(x.ravel() != 0)[0])
         y = y.reshape(-1, 1)
+
+        logger.info("Running IP.")
         log_ip = ip(Phi, y, debug=None)
+        logger.info("Running OMP.")
         log_omp = omp(Phi, y, debug=None)
 
         ip_recall = []
@@ -215,59 +250,63 @@ def main(m: int, n: int, s: float, output_dir: Path, device: str | None = None):
         ip_mse_x = []
         omp_mse_x = []
 
+        logger.info("Summarizing IP results.")
         for indices, x_hat, y_hat in zip(log_ip.indices, log_ip.x_hat, log_ip.y_hat):
             ip_recall.append(recall(indices, true_support))
             ip_precision.append(precision(indices, true_support))
             ip_mse_x.append(mse(x_hat, x))
             ip_mse_y.append(mse(y_hat, y))
 
+        logger.info("Summarizing OMP results.")
         for indices, x_hat, y_hat in zip(log_omp.indices, log_omp.x_hat, log_omp.y_hat):
             omp_recall.append(recall(indices, true_support))
             omp_precision.append(precision(indices, true_support))
             omp_mse_x.append(mse(x_hat, x))
             omp_mse_y.append(mse(y_hat, y))
 
-        plt.plot(omp_recall)
-        plt.plot(ip_recall, "--")
-        plt.legend(["OMP", "IP"])
-        plt.xlabel("Iteration")
-        plt.ylabel(
-            r"$\frac{|\mathrm{supp}(\widehat{x}) \, \cap \, \mathrm{supp}(x)|}{|\mathrm{supp}(x)|}$",
-            fontsize="x-large",
-        )
-        plt.title("Recall of estimated support")
-        plt.savefig(output_dir / "recall.png", dpi=300)
-        plt.close()
+        if plot:
+            logger.info("Generating single-run plots.")
+            plt.plot(omp_recall)
+            plt.plot(ip_recall, "--")
+            plt.legend(["OMP", "IP"])
+            plt.xlabel("Iteration")
+            plt.ylabel(
+                r"$\frac{|\mathrm{supp}(\widehat{x}) \, \cap \, \mathrm{supp}(x)|}{|\mathrm{supp}(x)|}$",
+                fontsize="x-large",
+            )
+            plt.title("Recall of estimated support")
+            plt.savefig(output_dir / "recall.png", dpi=300)
+            plt.close()
 
-        plt.plot(omp_precision)
-        plt.plot(ip_precision, "--")
-        plt.legend(["OMP", "IP"])
-        plt.xlabel("Iteration")
-        plt.ylabel(
-            r"$\frac{|\mathrm{supp}(\widehat{x}) \, \cap \, \mathrm{supp}(x)|}{|\mathrm{supp}(\widehat{x})|}$",
-            fontsize="x-large",
-        )
-        plt.title("Precision of estimated support")
-        plt.savefig(output_dir / "precision.png", dpi=300)
-        plt.close()
+            plt.plot(omp_precision)
+            plt.plot(ip_precision, "--")
+            plt.legend(["OMP", "IP"])
+            plt.xlabel("Iteration")
+            plt.ylabel(
+                r"$\frac{|\mathrm{supp}(\widehat{x}) \, \cap \, \mathrm{supp}(x)|}{|\mathrm{supp}(\widehat{x})|}$",
+                fontsize="x-large",
+            )
+            plt.title("Precision of estimated support")
+            plt.savefig(output_dir / "precision.png", dpi=300)
+            plt.close()
 
-        plt.plot(omp_mse_x)
-        plt.plot(ip_mse_x, "--")
-        plt.legend(["OMP", "IP"])
-        plt.xlabel("Iteration")
-        plt.ylabel(r"$\|x - \widehat{x}\|_2^2$")
-        plt.title("MSE of Sparse Code Estimate")
-        plt.savefig(output_dir / "mse_x.png", dpi=300)
-        plt.close()
+            plt.plot(omp_mse_x)
+            plt.plot(ip_mse_x, "--")
+            plt.legend(["OMP", "IP"])
+            plt.xlabel("Iteration")
+            plt.ylabel(r"$\|x - \widehat{x}\|_2^2$")
+            plt.title("MSE of Sparse Code Estimate")
+            plt.savefig(output_dir / "mse_x.png", dpi=300)
+            plt.close()
 
-        plt.plot(omp_mse_y)
-        plt.plot(ip_mse_y, "--")
-        plt.legend(["OMP", "IP"])
-        plt.xlabel("Iteration")
-        plt.ylabel(r"$\|y - \widehat{y}\|_2^2$")
-        plt.title("MSE of Measurement Estimate")
-        plt.savefig(output_dir / "mse_y.png", dpi=300)
-        plt.close()
+            plt.plot(omp_mse_y)
+            plt.plot(ip_mse_y, "--")
+            plt.legend(["OMP", "IP"])
+            plt.xlabel("Iteration")
+            plt.ylabel(r"$\|y - \widehat{y}\|_2^2$")
+            plt.title("MSE of Measurement Estimate")
+            plt.savefig(output_dir / "mse_y.png", dpi=300)
+            plt.close()
 
         results = {
             "precision_ip": ip_precision,
@@ -284,6 +323,7 @@ def main(m: int, n: int, s: float, output_dir: Path, device: str | None = None):
             "max_objective_omp": log_omp.objective,
         }
 
+        logger.info(f"Saving results to {output_dir / 'results.json'}")
         with open(output_dir / "results.json", "w") as f:
             json.dump(results, f)
 
