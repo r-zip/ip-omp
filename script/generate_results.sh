fddba6887c25dd411511cb1c10b623cb273da393
Ryan Pilgrim <rzpilgrim@gmail.com>
2023-08-03 17:52:06 -0400

    add noise

--- script/generate_results.sh
+++ script/generate_results.sh
@@ -4,22 +4,22 @@ python -m ip_is_all_you_need.simulations \
     --setting=small \
     --jobs=4 \
     --coeff-distribution=sparse_gaussian \
-    results_small_fixed_iter
+    results_small_noise_test
 
 python -m ip_is_all_you_need.simulations \
     --setting=large \
     --jobs=4 \
     --coeff-distribution=sparse_gaussian \
-    results_large_fixed_iter
+    results_large_noise_test
 
-python -m ip_is_all_you_need.simulations \
-    --setting=small \
-    --jobs=4 \
-    --coeff-distribution=sparse_const \
-    results_small_fixed_iter_const
+# python -m ip_is_all_you_need.simulations \
+#     --setting=small \
+#     --jobs=4 \
+#     --coeff-distribution=sparse_const \
+#     results_small_noise_test_const
 
-python -m ip_is_all_you_need.simulations \
-    --setting=large \
-    --jobs=4 \
-    --coeff-distribution=sparse_const \
-    results_large_fixed_iter_const
+# python -m ip_is_all_you_need.simulations \
+#     --setting=large \
+#     --jobs=4 \
+#     --coeff-distribution=sparse_const \
+#     results_large_noise_test_const
