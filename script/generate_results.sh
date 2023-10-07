71efaf660bdf1666f7d54e83cbd200c780e26d41
Ryan Pilgrim <rzpilgrim@gmail.com>
2023-10-07 10:17:32 -0400

    update generate_results.sh script

--- script/generate_results.sh
+++ script/generate_results.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-SUFFIX='rebuttal_final'
+SUFFIX='no_seed_change_stable'
 JOBS=16
 
-# python -m ip_is_all_you_need.simulations \
-#     --setting=small \
-#     --jobs=$JOBS \
-#     --coeff-distribution=sparse_gaussian \
-#     "results_small_${SUFFIX}"
+python -m ip_is_all_you_need.simulations \
+    --setting=small \
+    --jobs=$JOBS \
+    --coeff-distribution=sparse_gaussian \
+    "results_small_${SUFFIX}"
 
-# python -m ip_is_all_you_need.simulations \
-#     --setting=large \
-#     --jobs=$JOBS \
-#     --coeff-distribution=sparse_gaussian \
-#     "results_large_${SUFFIX}"
+python -m ip_is_all_you_need.simulations \
+    --setting=large \
+    --jobs=$JOBS \
+    --coeff-distribution=sparse_gaussian \
+    "results_large_${SUFFIX}"
 
 python -m ip_is_all_you_need.simulations \
     --setting=small \
