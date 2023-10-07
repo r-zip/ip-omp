8606453a9a2a74fe3ad213050929b6b65e86fb5d
Ryan Pilgrim <rzpilgrim@gmail.com>
2023-10-07 11:54:38 -0400

    update save location

--- script/generate_results.sh
+++ script/generate_results.sh
@@ -3,7 +3,8 @@
 SUFFIX='no_seed_change_stable'
 JOBS=8
 MACHINE='io52'
-OUTPUT_DIR="/export/${MACHINE}/data/rpilgri1"
+# OUTPUT_DIR="/export/${MACHINE}/data/rpilgri1"
+OUTPUT_DIR="./"
 
 python -m ip_is_all_you_need.simulations \
     --setting=small \
