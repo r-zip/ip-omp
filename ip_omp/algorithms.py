011d7c5930723ef863518600480b1747b6ebe1f4
Ryan Pilgrim <rzpilgrim@gmail.com>
2023-11-05 22:13:33 -0500

    remove unused functions

--- ip_omp/algorithms.py
+++ ip_omp/algorithms.py
@@ -9,42 +9,6 @@ from .constants import DEVICE
 logger = logging.getLogger(__name__)
 
 
-def projection(Phi_t: torch.Tensor, perp: bool = False, device: str | torch.device = DEVICE) -> torch.Tensor:
-    if Phi_t.numel() == 0 and perp:
-        return torch.eye(Phi_t.shape[1], device=device).repeat(Phi_t.shape[0], 1, 1)
-    elif Phi_t.numel() == 0 and not perp:
-        return torch.zeros((Phi_t.shape[0], Phi_t.shape[1], Phi_t.shape[1]), device=device)
-
-    U, *_ = torch.linalg.svd(Phi_t, full_matrices=False)
-    P = U @ U.transpose(1, 2)
-
-    if perp:
-        return torch.eye(P.shape[1], device=device)[None, :, :] - P
-
-    return P
-
-
-def estimate_y(Phi: torch.Tensor, y: torch.Tensor, indices, device: str | torch.device = DEVICE) -> torch.Tensor:
-    batches = torch.arange(Phi.shape[0], dtype=torch.long, device=device).reshape((-1, 1))
-    Phi_t = Phi[batches, :, indices].transpose(1, 2)
-    P = projection(Phi_t, perp=False, device=device)
-    return P @ y
-
-
-def estimate_x_and_y(Phi: torch.Tensor, y: torch.Tensor, indices, device: str | torch.device = DEVICE) -> torch.Tensor:
-    if indices is None:
-        return torch.zeros(Phi.shape[0], Phi.shape[2], 1, device=device)
-
-    batches = torch.arange(Phi.shape[0], dtype=torch.long, device=device).reshape((-1, 1))
-
-    Phi_t = Phi[batches, :, indices].transpose(1, 2)
-    x_hat = torch.zeros((Phi.shape[0], Phi.shape[2], 1), device=device)
-
-    # equivalent to left-multiplying y by pseudoinverse of Phi_t
-    x_hat[batches, indices] = torch.linalg.lstsq(Phi_t, y).solution
-    return x_hat, Phi @ x_hat
-
-
 def omp(
     Phi: torch.Tensor,
     y: torch.Tensor,
