dae684b047b05689495e371428be02fdb5e366c0
Ryan Pilgrim <rzpilgrim@gmail.com>
2023-12-10 20:07:34 -0500

    fix custom_dataset paths

--- ip_omp/custom_dataset.py
+++ ip_omp/custom_dataset.py
@@ -4,14 +4,16 @@ import numpy as np
 import torch
 import torchvision.datasets as datasets
 
+from .constants import MODULE_DIR
+
 
 class Places365_clip(datasets.Places365):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.small and self.split == "train-standard":
-            self.attr_dir = os.path.join("./data/Places365_clip/train-standard/")
+            self.attr_dir = str(MODULE_DIR / "data/Places365_clip/train-standard/")
         elif self.small and self.split == "val":
-            self.attr_dir = os.path.join("./data/Places365_clip/val_256/")
+            self.attr_dir = str(MODULE_DIR / "data/Places365_clip/val_256/")
 
     def __getitem__(self, idx):
         label = self.imgs[idx][1]
